c59d7484bdbad1728603f1c42f7baf9861a5a476
lamtriphuc <lamtriphuc2004@gmail.com>
2025-10-21 23:51:24 +0700

    fix db, add gdp

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import countryRoutes from './country.route.js';
+import gdpRoutes from './gdp.route.js';
 
 const router = express.Router();
 
 router.use('/countries', countryRoutes);
+router.use('/gdp', gdpRoutes);
 
 export default router;
--- frontend/src/components/Cards/StatCard.jsx
+++ frontend/src/components/Cards/StatCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const StatCard = ({ title, subtitle, value, icon: Icon, iconColor, bgColor }) => {
     return (
         <div>
-            <div className="p-6 bg-white shadow-lg hover:shadow-xl transition-shadow rounded-xl">
+            <div className="p-6 bg-white dark:bg-gray-800 shadow-lg hover:shadow-xl transition-shadow rounded-xl">
                 <div className="flex items-center justify-between">
                     <div>
                         <p className="text-gray-600 mb-2">{title}</p>
                         <div className='flex gap-2'>
-                            <p className="text-gray-900 text-2xl font-medium">{value}</p>
+                            <p className="text-gray-900 dark:text-gray-300 text-2xl font-medium">{value}</p>
                             {subtitle && <p className="text-gray-500 text-sm mt-1">{subtitle}</p>}
                         </div>
                     </div>
