1f52123d23f9dea3e2ef095f5af45f463b8f486c
Phat <2251120435@ut.edu.vn>
2025-10-27 23:48:00 +0700

    translate country detail page, fix search bar

--- frontend/src/components/Cards/StatCard.jsx
+++ frontend/src/components/Cards/StatCard.jsx
@@ -11,7 +11,7 @@ const StatCard = ({
 	return (
 		<div>
 			<div className='p-6 bg-white dark:bg-gray-800 shadow-lg hover:shadow-xl transition-shadow rounded-xl'>
-				<div className='flex items-center justify-between'>
+				<div className='flex flex-wrap items-center justify-between'>
 					<div>
 						<p className='text-gray-600 dark:text-gray-300 mb-2'>{title}</p>
 						<div className='flex gap-2'>
