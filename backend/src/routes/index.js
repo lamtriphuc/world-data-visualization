97ede0e1115b5ccb019de82438ec9d568720aa22
lamtriphuc <lamtriphuc2004@gmail.com>
2026-01-02 09:25:31 +0700

    remove trash

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import countryRoutes from './country.route.js';
 import authRoutes from './auth.route.js';
 import regionRoutes from './region.route.js';
-import recommendationRoutes from './recommendation.route.js';
 import userRoutes from './user.route.js';
 import smartSearchRoutes from './smartSearch.route.js';
 import aiRoutes from './ai.route.js';
@@ -13,7 +12,6 @@ router.use('/countries', countryRoutes);
 router.use('/auth', authRoutes);
 router.use('/region', regionRoutes);
 router.use('/search', smartSearchRoutes);
-router.use('/recommendations', recommendationRoutes);
 router.use('/user', userRoutes);
 router.use('/ai', aiRoutes);
 
